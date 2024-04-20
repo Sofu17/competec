9ecb3794152573ca4197e3282d3754ed32b283c3
Sofia <166915743+Sofu17@users.noreply.github.com>
2024-04-20 10:04:05 -0300

    Exercicio1.java SOFIA

--- variaveis/Exercicio1.java
+++ variaveis/Exercicio1.java
@@ -3,7 +3,9 @@
 2. Crie uma variável que informa a sua idade;
 3. Crie uma variável informando se você tomou café da manhã;
 */
-
+char PrimeiraLetra='S
+int Idade= 16
+boolean TomeiCafe= Sim  
 public class Exercicio1 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
